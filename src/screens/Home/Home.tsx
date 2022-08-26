8b9b187a5766670e20b9e2e615323403a95e898b
Frank <55867157+thedraxx@users.noreply.github.com>
2022-08-26 14:22:01 -0300

    interface api

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react';
 import { Text, View} from 'react-native';
 import movieDB from '../../api/movieDB/MovieDB';
+import { MovieDBNowPlaying } from '../../interfaces/MovieInterface';
 
 const Home = () => {
 
   useEffect(() => {
     
-    movieDB.get('/now_playing')
+    movieDB.get<MovieDBNowPlaying>('/now_playing')
       .then(resp => {
-        console.log(resp.data)
+        console.log(resp.data.results[0].title)
       })
 
   }, [])
