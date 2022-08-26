0b268869c370ad00f51f5242c4327f4c593470cb
Frank <55867157+thedraxx@users.noreply.github.com>
2022-08-26 16:42:59 -0300

    add carousel

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,9 +1,12 @@
+import Carousel from 'react-native-snap-carousel';
 import React from 'react';
-import { ActivityIndicator, Text, View} from 'react-native';
+import { ActivityIndicator, Dimensions, Text, View} from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Cards from '../../components/Card/Cards';
 import useMovies from '../../hooks/useMovies';
 import { HomeView, Loading } from './style';
+
+const {width} = Dimensions.get('window')
 
 const Home = () => {
 const {moviesInCinema,IsLoading} = useMovies()
@@ -20,8 +23,11 @@ const {top} = useSafeAreaInsets()
 
   return (
     <HomeView top={top} inputColor="rebeccapurple">
-      <Cards 
-          movie={moviesInCinema[0]}
+      <Carousel
+          data={moviesInCinema}
+          renderItem={({item}:any) => <Cards movie={item} /> }
+          sliderWidth={width}
+          itemWidth={300}
       />
     </HomeView>
   );
