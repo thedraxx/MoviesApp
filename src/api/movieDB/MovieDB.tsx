82d3d329099b9522f870b6db0c97e004e88bfd53
Frank <55867157+thedraxx@users.noreply.github.com>
2022-08-26 14:12:23 -0300

    add AXIOS and conect API

---
+++ src/api/movieDB/MovieDB.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import axios from 'axios';
+
+const movieDB = axios.create({
+    baseURL: `https://api.themoviedb.org/3/movie`,
+    params:{
+        api_key: 'c4b768c3f1af19bd3579d1f4c4903296',
+        language: 'en-US'
+    }
+});
+
+export default movieDB
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, View} from 'react-native';
+import movieDB from '../../api/movieDB/MovieDB';
 
 const Home = () => {
+
+  useEffect(() => {
+    
+    movieDB.get('/now_playing')
+      .then(resp => {
+        console.log(resp.data)
+      })
+
+  }, [])
+
   return (
     <View>
       <Text>HomeScren</Text>
