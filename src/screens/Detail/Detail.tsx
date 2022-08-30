e5d9cd94f3e135cb090df991cca950c4697b7d94
thedraxx <thedraxxgames@gmail.com>
2022-08-30 15:53:45 -0300

    add vector icons

--- src/screens/Detail/Detail.tsx
+++ src/screens/Detail/Detail.tsx
@@ -3,10 +3,14 @@ import React from 'react';
 import { RootStackParams } from '../../navigation/Navigation';
 import { ContainerImage, ContainerInfo, ImagePoster, Title, SubTitle } from './style';
 import { ScrollView, Dimensions } from 'react-native';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
 const screenHeight = Dimensions.get('screen').height;
+
 interface Props extends StackScreenProps<RootStackParams, 'Detail'> { }
+
 const Detail = ({ route }: Props) => {
+
   const movie = route.params;
   const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
 
@@ -22,6 +26,11 @@ const Detail = ({ route }: Props) => {
         <Title>{movie.original_title}</Title>
         <SubTitle>{movie.title}</SubTitle>
       </ContainerInfo>
+
+      <ContainerInfo>
+        <Icon name="star" color="gray" size={20} ></Icon>
+      </ContainerInfo>
+
     </ScrollView>
   );
 };
