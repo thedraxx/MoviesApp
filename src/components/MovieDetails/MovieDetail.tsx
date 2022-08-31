eb13c8e6421b7eae400e6ff7abc8966d5deba3ad
thedraxx <thedraxxgames@gmail.com>
2022-08-31 14:30:44 -0300

    detail of the movie 2

--- src/components/MovieDetails/MovieDetail.tsx
+++ src/components/MovieDetails/MovieDetail.tsx
@@ -3,8 +3,8 @@ import { View, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { Cast } from '../../interfaces/creditsInterface';
 import { MovieFull } from '../../interfaces/MovieInterface';
-import { ContainerAverage, Genres, NumberAverage } from './style';
-
+import { ContainerAverage, Genres, NumberAverage, TextOverview, TextInfo } from './style';
+import currencyFormatter from "currency-formatter";
 interface Props {
     movieFull: MovieFull
     cast: Cast[]
@@ -18,18 +18,29 @@ const MovieDetail = ({ movieFull, cast }: Props) => {
                 <ContainerAverage>
                     <Icon name="star-outline" color="gray" size={16} />
                     <NumberAverage>{movieFull.vote_average}</NumberAverage>
-                    
-                    <Genres>
-                         - {movieFull.genres.map(g => g.name).join('-')}
-                    </Genres>
 
+                    <Genres>
+                        - {movieFull.genres.map(g => g.name).join('-')}
+                    </Genres>
                 </ContainerAverage>
             </View>
 
+            {/* Plot */}
+
+            <TextInfo style={{color:'black'}}>Overview</TextInfo>
 
             {/* Casting */}
+            <TextOverview>{movieFull.overview}</TextOverview>
 
-            <Text>{movieFull.title}</Text>
+            <TextInfo style={{color:'black'}}>Budget</TextInfo>
+            {/* Budget */}
+            <Text>
+            {
+                movieFull.budget === 0 ? <Text>N/A</Text> : currencyFormatter.format(movieFull.budget,{code:'USD'})
+            }
+
+            
+            </Text>
 
         </>
 
--- src/components/MovieDetails/style.tsx
+++ src/components/MovieDetails/style.tsx
@@ -1,17 +1,28 @@
 import styled from 'styled-components/native';
+import { Colors } from '../../utilities/colors/colors';
 
 export const ContainerAverage = styled.View`
     display: flex;
     flex-direction: row;
 `;
 
-export const NumberAverage = styled.Text `
+export const NumberAverage = styled.Text`
     margin-left: 5px;
 
 `
 
-export const Genres = styled.Text `
+export const Genres = styled.Text`
     margin-left: 8px;
 
+`
+
+export const TextInfo = styled.Text`
+    font-size: 23px;
+    margin-top: 10px;
+    font-weight: bold;
+`
+
+export const TextOverview = styled.Text`
+    font-size: 16px;
 `
 
